63b6463d4243ff66da1d9e2f97ed5b1214bee261
danppo <danielpocock@gmail.com>
2022-02-16 09:24:54 +0000

    WIP create auth middleware

--- server/src/routes/register.js
+++ server/src/routes/register.js
@@ -40,7 +40,18 @@ const passwordHash = async (password, action, savedHash) => {
 
 };
 
+const authenticateToken = (req, res , next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1]
+  if (token === null) return res.sendStatus(401)
 
+  jwt.verify(token, process.env.TOKEN_KEY, (err, user) => {
+    if (err) return res.sendStatus(403)
+    req.user = user
+    next()
+
+  })
+}
 
 
 const router = express.Router();
